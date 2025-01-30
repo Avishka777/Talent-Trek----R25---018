0d9408203c4ee490ec3dd66ced1feab14b132368
Avishka777 <a.rathnakumara777@gmail.com>
2025-01-31 02:10:12 +0530

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,73 @@
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { useSelector, useDispatch } from "react-redux";
+import { FaSun, FaMoon } from "react-icons/fa";
+import { toggleTheme } from "../redux/theme/themeSlice";
+import logo from "../assets/public/logo.png";
+
+export default function HeaderComponent() {
+  const { theme } = useSelector((state) => state.theme);
+  const dispatch = useDispatch();
+
+  const handleThemeToggle = () => {
+    dispatch(toggleTheme());
+  };
+
+  return (
+    <Navbar fluid rounded>
+      <Navbar.Brand href="#">
+        <img src={logo} className="mr-3 h-6 sm:h-9" alt="TALENT TREK LOGO" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          TALENT TREK
+        </span>
+      </Navbar.Brand>
+      <div className="flex md:order-2 items-center gap-4">
+        {/* Theme Toggle Button */}
+        <button
+          onClick={handleThemeToggle}
+          className="p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none"
+        >
+          {theme === "light" ? (
+            <FaMoon className="w-6 h-6 text-gray-700 dark:text-white" />
+          ) : (
+            <FaSun className="w-6 h-6 text-gray-700 dark:text-white" />
+          )}
+        </button>
+
+        {/* Profile Dropdown */}
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#">About</Navbar.Link>
+        <Navbar.Link href="#">Services</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
