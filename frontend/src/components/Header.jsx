e91a6ec09c6d266765e43b1459333ee165d58e80
Avishka777 <a.rathnakumara777@gmail.com>
2025-01-31 23:04:58 +0530

    CHange Name And Fav Icon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -63,10 +63,8 @@ export default function HeaderComponent() {
         <Navbar.Link href="#" active>
           Home
         </Navbar.Link>
-        <Navbar.Link href="#">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
+        <Navbar.Link href="sign-in">Signin</Navbar.Link>
+        <Navbar.Link href="sign-up">Signup</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
